23c2dc6ad772f4fbad9b2f7595d608d77ead7cc9
Marco Pariente-Cohen <markypc3@gmail.com>
2014-10-26 23:50:00 +0000

    first commit

---
+++ hello-cpp-world.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "math.h"
+
+using namespace std;
+int main() {
+    cout << "Hello World!" << endl;
+    int a = 0;
+    cin >> a;
+    cout << a<< " Squared is " << pow(a,2) << endl;
+}
