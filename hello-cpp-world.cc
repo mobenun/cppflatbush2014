118782829d3836c0bdf92e06f70ba5e7dd4e1a13
mobenun <mobenun@aol.com>
2014-11-03 19:09:44 +0000

    first personal commit

--- hello-cpp-world.cc
+++ hello-cpp-world.cc
@@ -6,5 +6,5 @@ int main() {
     cout << "Hello World!" << endl;
     int a = 0;
     cin >> a;
-    cout << a<< " Squared is " << pow(a,2) << endl;
+    cout << a<< " Cubed is " << pow(a,3) << endl;
 }
